0633cff287d025a16b18f80355adcc12848065f4
XRoldo <xavier.roldo@elao.com>
2015-12-06 15:31:20 +0100

    Display committed lines counters

--- domain/treeRenderer.js
+++ domain/treeRenderer.js
@@ -18,6 +18,11 @@
         folderContainer.className = "folder-container";
         var folderTitle           = document.createElement('h2');
         folderTitle.textContent   = folder.name;
+
+        var counters = this.renderCounters(folder);
+        for (var i = 0 ; i < counters.length ; i++) {
+            folderTitle.appendChild(counters[i]);
+        }
         folderContainer.appendChild(folderTitle);
 
         for (key in folder.folders) {
@@ -44,11 +49,35 @@
             var gitfile  = folder.files[keys[key]];
             var fileItem = document.createElement('li');
             fileItem.textContent = gitfile.name;
+            var counters = this.renderCounters(gitfile);
+            for (var i = 0 ; i < counters.length ; i++) {
+                fileItem.appendChild(counters[i]);
+            }
             fileList.appendChild(fileItem);
         }
 
         return fileList;
     };
+
+    TreeRenderer.prototype.renderCounters = function(node) {
+        var spans = [];
+
+        if (node.created > 0) {
+            var span = document.createElement('span');
+            span.className = 'counter counter-created';
+            span.textContent = node.created;
+            spans.push(span);
+        }
+
+        if (node.removed > 0) {
+            var span = document.createElement('span');
+            span.className = 'counter counter-removed';
+            span.textContent= node.removed;
+            spans.push(span);
+        }
+
+        return spans;
+    }
 
     global.TreeRenderer = TreeRenderer;
 
